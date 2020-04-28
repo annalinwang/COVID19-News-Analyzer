4d074522ca5459e1e22f4d2431a3816ba686dbdc
Anna Wang <38119775@users.noreply.github.com>
2020-04-27 23:55:46 -0700

    delete duplicate

--- NETS150hw4/src/Main.java
+++ NETS150hw4/src/Main.java
@@ -7,12 +7,10 @@ public class Main {
     public static void main(String[] args) {
         
         ArticleSorter articleSorter = new ArticleSorter();
-//        Set<Article> articles = articleSorter.getArticlesInRegion("americas");
-//        Set<Article> articles = articleSorter.getAllArticles();
-//        Set<Article> articles = articleSorter.getArticlesInRegion("americas");
+        Set<Article> articles = articleSorter.getArticlesInRegion("americas");
 //        Set<Article> articles = articleSorter.getAllArticles();
 //        Set<Article> articles = articleSorter.getArticlesInRegionOnDayOfWeek("south-east asia", DayOfWeek.TUESDAY);
-        Set<Article> articles = articleSorter.getArticlesWithTitleContaining("trump");
+//        Set<Article> articles = articleSorter.getArticlesWithTitleContaining("trump");
         
         System.out.println();
         System.out.println(articles.size() + " articles found.");
